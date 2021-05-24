9db99cfe9adc91a0fcb496137a1395986d8b3d9b
asasdll <30594995+asasdll@users.noreply.github.com>
2021-05-25 01:18:57 +0700

    Up_note

--- assets/js/function.js
+++ assets/js/function.js
@@ -43,15 +43,13 @@ function draw_notes() {
   stave.addClef("treble").addTimeSignature("4/4");
 
 
-  voice = new VF.Voice({ num_beats: 7, beat_value: 4 });
+  voice = new VF.Voice({ num_beats: 5, beat_value: 4 });
   notes = [
-    get_new_note('a', 4, 'q'),
-    get_new_note('b', 4, 'q'),
-    get_new_note('c', 4, 'q'),
+    get_new_note('a', 4, 'qr'),
+    get_new_note('b', 4, 'qr'),
+    get_new_note('c', 4, 'qr'),
     get_new_note('d', 4, 'qr'),
-    get_new_note('e', 4, 'qr'),
-    get_new_note('f', 4, 'qr'),
-    get_new_note('g', 4, 'qr')
+    get_new_note('d', 4, 'qr')
   ];
 
   voice.addTickables(notes);
@@ -67,7 +65,7 @@ function draw_notes() {
 
 
 function redraw_notes() {
-  voice = new VF.Voice({ num_beats: 7, beat_value: 4 });
+  voice = new VF.Voice({ num_beats: 5, beat_value: 4 });
 
   voice.addTickables(notes);
   var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
@@ -81,15 +79,22 @@ function redraw_notes() {
 
 
 function add_note(key, octave, duration) {
-
-  // var idx = "0";
-  var key = key;
-  var octave = octave;
-  var duration = duration;
-  notes[0] = get_new_note(key, octave, duration);
-  redraw_notes();
   i_number();
+  //console.log(key, octave, duration);
+  if (i < notes.length) {
+    var idx = i;
+    var key = key;
+    var octave = octave;
+    var duration = duration;
+    notes[idx] = get_new_note(key, octave, duration);
+    redraw_notes();
 
+  } else {
+    i = -1;
+    
+
+  }
+  console.log(i);
 
 }
 
@@ -105,31 +110,26 @@ function arrindex() {
 
 function mouseDown() {
   $(".vf-stavenote").on("click", function (e) {
-   
+
     arrindex();
 
     var arr_index = $(this).attr("arr-index");
     var id_y = $(this).attr("id");
-    
+
     //notes[arr_index] = get_new_note('c', '4', 'q').setKeyStyle(2, {shadowColor: "yellow", shadowBlur: 3});
     notes[arr_index].setStyle({ fillStyle: "OrangeRed", strokeStyle: "Black" });
-    
+
     redraw_notes();
 
-    $( function() {
+    $(function () {
       $("#" + id_y).draggable({ axis: "y" });
-    } );
+    });
 
     $("div")
       .mouseup(function () {
         $(this).append(notes[arr_index].setStyle({ fillStyle: "Black", strokeStyle: "Black" }));
       });
-     
 
-      
-      //var top_y = $(this).attr("style");
-    //  console.log(id_y);
-  
 
 
   })
@@ -139,7 +139,6 @@ function mouseDown() {
 function i_number() {
   i++;
   var value = document.querySelector(".increment-btn");
-  
 }
 
 
