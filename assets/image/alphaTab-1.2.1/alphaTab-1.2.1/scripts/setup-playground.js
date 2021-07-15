7443273d49b8a6c381488aa646d078914fcf4d6e
asasdll <Lollipopwar123>
2021-07-15 15:35:40 +0700

    image Common time

--- assets/image/alphaTab-1.2.1/alphaTab-1.2.1/scripts/setup-playground.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs-extra');
-
-const src = `${__dirname}/../playground-template`
-const dest = `${__dirname}/../playground`
-fs.copySync(src, dest, { overwrite: false });
--- assets/image/alphaTab-1.2.1/alphaTab-1.2.1/src.csharp/AlphaTab/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("AlphaTab.Test")]
-[assembly: InternalsVisibleTo("AlphaTab.Windows")]
