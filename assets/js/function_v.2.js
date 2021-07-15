a7b821ae7063ba86ec2e682792d1799b845b3abe
asasdll <Lollipopwar123>
2021-07-15 13:43:39 +0700

    up_code

--- assets/js/function_v.2.js
+++ assets/js/function_v.2.js
@@ -307,6 +307,7 @@ function modifyStave() {
 
 
 function mouseDown() {
+  console.log("222");
   $(".vf-stavenote")
     .mousedown(function (e) {
 
@@ -615,37 +616,11 @@ function time_Signature() {
   });
 }
 
-function frame_Ts() {
-
-  var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-//var btn = document.querySelectorAll("path");
-//var btn = document.querySelectorAll($(this).attr("id"));
-//var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+function  pop_time_Signature(e) {
+  
+  console.log(e);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-}
 
 
 
