52055885327d7ef2e377c4a7514e1575ac0940f8
asasdll <Lollipopwar123>
2021-07-15 09:18:30 +0700

    up_code

--- assets/js/function_v.2.js
+++ assets/js/function_v.2.js
@@ -400,18 +400,18 @@ function mouseDown() {
 
     $("path")
     .mousedown(function (e) {
+      var myModal = document.getElementById('exampleModal')
+     /* var myInput = document.getElementById('myInput')
       
+      myModal.addEventListener('shown.bs.modal', function () {
+        myInput.focus()
+      })*/
+      if (myModal != null) {
+        console.log("sdsad");
+      }
+ 
 
-      $('#exampleModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var recipient = button.data('whatever') // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this)
-        modal.find('.modal-title').text('New message to ' + recipient)
-        modal.find('.modal-body input').val(recipient)
-      })
-      
+
     });
 
 
