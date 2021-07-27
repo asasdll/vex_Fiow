af890c015d593db69021dd20848bb506d172851c
asasdll <Lollipopwar123>
2021-07-27 13:24:12 +0700

    upimage

---
+++ assets/js/button.js
@@ -0,0 +1,23 @@
+function bigImg(x) {
+    x.style.border = "1px solid gray";
+    x.style.width = "35px";
+  
+    x.style.margin = "0px";
+    x.style.position = "static";
+ 
+  }
+  
+  function normalImg(x) {
+    x.style.border = "";
+  }
+
+  function bigNote(x) {
+    x.style.border = "1px solid gray";
+    x.style.width = "25px";
+  
+    x.style.margin = "0px";
+    x.style.position = "static";
+ 
+  }
+  
+  
